71907ff6c8d177b392c813ece204c27c322bb25c
Washwill <104589724+Washwill@users.noreply.github.com>
2023-03-04 11:50:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,40 @@ desenho = document.getElementById("desenho").innerHTML = sketch;
 console.log(desenho);
 //juntar = desenho + sketch;
 //console.log(juntar);
+
+function draw(){
+    checkSketch();
+    if(drawSketch = sketch){
+        answerHolder = "set";
+        score = score + 1;
+        pontuacao = document.getElementById("pontuacao").innerHTML = pontos.concat(score);
+    }
+}
+
+function checkSketch(){
+    timerCounter = timerCounter + 1;
+    velocidade = document.getElementById("velocidade").innerHTML = tempo.concat(timerCounter);
+    console.log(timerCounter);
+    if(timerCounter > 400){
+        velocidade > 400;
+        timerCounter = 0;
+        timerCheck = "completed";
+    }
+    if(timerCheck = "completed"){
+        timerCheck = "";
+        answerHolder = "";
+        updateCanvas();
+    }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+
+function updateCanvas(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
